895d1f673f94cf7dbd18a6c7a5a658c8a5a44fd7
mlee <mlee@student.42seoul.kr/>
2020-01-29 14:50:16 +0900

    edit

--- songs/ft_strcapitalize.c
+++ songs/ft_strcapitalize.c
@@ -6,7 +6,7 @@ char		*ft_strcapitalize(char *str)
 	int q;
 
 	i = 0;
-	q = 0;
+	q = 1;
 	while (str[i] != '\0')
 	{
 		if ((str[i] >= 32 && str[i] <= 47) || (str[i] >= 58 && str[i] <= 64) || (str[i] >= 123 && str[i] <= 126))
@@ -14,14 +14,14 @@ char		*ft_strcapitalize(char *str)
 		else if ((q = 1) && (str[i] >= 'a' && str[i] <= 'z'))
 		{
 			str[i] = str[i] - 32;
-			q++;
+			q = 0;
 		}
 		else if (str[i] >= 48 && str[i] <= 57)
-			q++;
+			q = 0;
 		else if ((q != 1) && (str[i] >= 'A' && str[i] <= 'Z'))
 			str[i] = str[i] + 32;
 		else if (str[i] >= 'A' && str[i] <= 'Z')
-			q++;
+			q = 0;
 		i++;
 	}
 	return (str);
