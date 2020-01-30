9b00a37e19b709609d92e951a0accea33c006800
mlee <mlee@student.42seoul.kr/>
2020-01-30 21:31:23 +0900

    songsong

---
+++ C03/ex01/ft_strncmp.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlee <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/01/30 21:20:48 by mlee              #+#    #+#             */
+/*   Updated: 2020/01/30 21:30:58 by mlee             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <string.h>
+
+int	ft_strncmp(char *s1, char *s2, unsigned int n)
+{
+	int i;
+
+	i = 0;
+	while ((s1[i] != '\0' || s2[i] != '\0') && (i < n))
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+	return (0);
+}
+
+int main()
+{
+	char a[100] = "hello";
+	char b[] = "hello world";
+	printf("%d\n", ft_strncmp(a, b, 6));
+	printf("%d\n", strncmp(a, b, 6));
+}
